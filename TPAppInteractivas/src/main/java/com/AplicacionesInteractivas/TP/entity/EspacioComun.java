fd2ec007bbad8455c9fa74cfd30d0b251fd483a2
Tissera Rodrigo <rodrigo.tissera@supervielle.com.ar>
2020-10-15 19:45:07 -0300

    correccion de ID

--- TPAppInteractivas/src/main/java/com/AplicacionesInteractivas/TP/entity/EspacioComun.java
+++ TPAppInteractivas/src/main/java/com/AplicacionesInteractivas/TP/entity/EspacioComun.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -21,6 +23,9 @@ public class EspacioComun {
 	@Column(name="descripcion")
 	private String descripcion;
 	
+	@ManyToOne
+	@JoinColumn(name="id_edificio", nullable = true)
+	private Edificio edificio;
 	
 	public EspacioComun() {
 		super();
